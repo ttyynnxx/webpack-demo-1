49ff42161a6b8bbd9a8458b8cc17c65b52d957eb
Smiletian <ttyy373172968@163.com>
2020-10-13 21:33:04 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.[contenthash].js'
   },
+ 
   plugins: [new HtmlWebpackPlugin({
     title: '我要写代码啦',
     template: 'src/assets/test.html'
