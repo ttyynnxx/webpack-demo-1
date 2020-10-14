2cf7bc94e0c93752c63cebdd5bd5f177beca19c5
Smiletian <ttyy373172968@163.com>
2020-10-14 21:53:49 +0800

    update 1014

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import x from './x.js'
+import png from './assets/2.png'
 console.log('x')
 console.log('hi')
+
+const div =document.getElementById('APP')
+
+div.innerHTML=`
+<img src="${png}">
+`
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,4 +14,57 @@ module.exports = {
     title: '我要写代码啦',
     template: 'src/assets/test.html'
   })],
+  module: {
+    rules: [
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('dart-sass'),
+            }
+        },
+        ],
+      },  
+      {
+        test: /\.less$/,
+        use: [{
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+          },
+          
+        ], // compiles Less to CSS
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'stylus-loader', // compiles Stylus to CSS
+          },
+        ],
+      },
+    ],
+  },
 };
