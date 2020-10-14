792c866180b6b30a793433d29be73656f934b577
Smiletian <ttyy373172968@163.com>
2020-10-15 06:20:39 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ const div =document.getElementById('APP')
 div.innerHTML=`
 <img src="${png}">
 `
+
+const button = document.createElement('button')
+button.innerText='懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn =module.default
+        fn()
+    },()=>{
+        console.log('模块加载失败')
+    })
+}
+
+div.appendChild(button)
